a76dc4e647deb179edd5259ead86701a2031fd11
drzzln <drzzln@protonmail.com>
2026-03-15 19:56:46 -0400

    add nix flake with substrate integration

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "OpenSSL fork - C cryptographic library with TLS/SSL implementation";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          gcc
+          gnumake
+          autoconf
+          automake
+          libtool
+          perl
+          pkg-config
+        ];
+      };
+    });
+}
