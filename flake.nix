ede889458d4a1a108101696a00c9b46e8be38ad0
drzzln <drzzln@protonmail.com>
2026-03-15 21:10:03 -0400

    refactor: use substrate repo-flake.nix universal builder
    Replaces bespoke flake.nix boilerplate with declarative config via
    substrate's repo-flake.nix. All build logic, devShell tooling, and
    meta attributes derived from (language, builder) dispatch.

--- flake.nix
+++ flake.nix
@@ -1,23 +1,21 @@
 {
   description = "OpenSSL fork - C cryptographic library with TLS/SSL implementation";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    substrate = {
+      url = "github:pleme-io/substrate";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells.default = pkgs.mkShellNoCC {
-        packages = with pkgs; [
-          gcc
-          gnumake
-          autoconf
-          automake
-          libtool
-          perl
-          pkg-config
-        ];
-      };
-    });
+
+  outputs = inputs:
+    (import "${inputs.substrate}/lib/repo-flake.nix" {
+      inherit (inputs) nixpkgs flake-utils;
+    }) {
+      self = inputs.self;
+      language = "c";
+      description = "OpenSSL fork - C cryptographic library with TLS/SSL implementation";
+    };
 }
